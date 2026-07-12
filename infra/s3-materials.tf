0d0ce5ae6c6579d8473dd32c6bb26ed69dea0642
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-11 19:17:37 -0500

    Conservar versiones de los archivos guardados

--- infra/s3-materials.tf
+++ infra/s3-materials.tf
@@ -14,3 +14,11 @@ resource "aws_s3_bucket_public_access_block" "materials" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_versioning" "materials" {
+  bucket = aws_s3_bucket.materials.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
