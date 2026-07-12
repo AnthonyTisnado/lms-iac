5b7538a726bea3908cf144da8b2bc8763751c2b2
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-11 19:18:32 -0500

    Cifrar los archivos guardados en S3

--- infra/s3-materials.tf
+++ infra/s3-materials.tf
@@ -22,3 +22,13 @@ resource "aws_s3_bucket_versioning" "materials" {
     status = "Enabled"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "materials" {
+  bucket = aws_s3_bucket.materials.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
