a5cf72431cd1801f12d504c96891de8f4f04ca9d
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-11 19:05:07 -0500

    Preparar las rutas de las subredes privadas

---
+++ infra/private-routes.tf
@@ -0,0 +1,12 @@
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.main.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-rutas-privadas"
+  }
+}
