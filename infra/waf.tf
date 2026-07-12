1b215f162f69b21f0dd4f8235521be460e883c3d
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-11 19:29:05 -0500

    Agregar reglas comunes de proteccion web

--- infra/waf.tf
+++ infra/waf.tf
@@ -6,6 +6,28 @@ resource "aws_wafv2_web_acl" "frontend" {
     allow {}
   }
 
+  rule {
+    name     = "reglas-comunes"
+    priority = 1
+
+    override_action {
+      none {}
+    }
+
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesCommonRuleSet"
+        vendor_name = "AWS"
+      }
+    }
+
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "reglas-comunes"
+      sampled_requests_enabled   = true
+    }
+  }
+
   visibility_config {
     cloudwatch_metrics_enabled = true
     metric_name                = "${var.project_name}-${var.environment}-frontend"
