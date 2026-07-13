2d67f4e36b1a4def908268fb2845c775821d3468
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-13 18:32:53 -0500

    feat: crear el grupo de alumnos en Cognito

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -41,3 +41,8 @@ resource "aws_cognito_user_group" "professor" {
   name         = "PROFESOR"
   user_pool_id = aws_cognito_user_pool.main.id
 }
+
+resource "aws_cognito_user_group" "student" {
+  name         = "ALUMNO"
+  user_pool_id = aws_cognito_user_pool.main.id
+}
