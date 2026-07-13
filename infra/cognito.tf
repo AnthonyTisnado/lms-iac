4da722f79716c6a4a3169eedb63626f2f73aca4c
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-13 17:50:56 -0500

    feat: crear el cliente de Cognito para el frontend

--- infra/cognito.tf
+++ infra/cognito.tf
@@ -8,3 +8,14 @@ resource "aws_cognito_user_pool" "main" {
     Name = "${var.project_name}-${var.environment}-usuarios"
   }
 }
+
+resource "aws_cognito_user_pool_client" "frontend" {
+  name         = "${var.project_name}-${var.environment}-frontend"
+  user_pool_id = aws_cognito_user_pool.main.id
+
+  generate_secret = false
+  explicit_auth_flows = [
+    "ALLOW_USER_PASSWORD_AUTH",
+    "ALLOW_REFRESH_TOKEN_AUTH"
+  ]
+}
