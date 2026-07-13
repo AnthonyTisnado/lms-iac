5824034a947ead918dbb8274c6932a3214f419b5
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-13 16:30:24 -0500

    feat: permitir el acceso privado al balanceador

--- infra/security-api-gateway.tf
+++ infra/security-api-gateway.tf
@@ -7,3 +7,11 @@ resource "aws_security_group" "api_gateway_link" {
     Name = "${var.project_name}-${var.environment}-api-link-sg"
   }
 }
+
+resource "aws_vpc_security_group_egress_rule" "api_gateway_to_alb" {
+  security_group_id            = aws_security_group.api_gateway_link.id
+  referenced_security_group_id = aws_security_group.alb.id
+  from_port                    = 80
+  to_port                      = 80
+  ip_protocol                  = "tcp"
+}
