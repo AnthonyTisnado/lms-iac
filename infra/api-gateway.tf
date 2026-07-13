27350ccaaf8e70de2b6e6198c6f9e12d78de246c
alexmisaellgonzalessoto <151983847+alexmisaellgonzalessoto@users.noreply.github.com>
2026-07-13 16:31:40 -0500

    feat: crear el enlace privado de la API

--- infra/api-gateway.tf
+++ infra/api-gateway.tf
@@ -6,3 +6,13 @@ resource "aws_apigatewayv2_api" "main" {
     Name = "${var.project_name}-${var.environment}-api"
   }
 }
+
+resource "aws_apigatewayv2_vpc_link" "main" {
+  name               = "${var.project_name}-${var.environment}-vpc-link"
+  security_group_ids = [aws_security_group.api_gateway_link.id]
+  subnet_ids         = aws_subnet.private[*].id
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-vpc-link"
+  }
+}
