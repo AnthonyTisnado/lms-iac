e0cdeb58168c5e53192e325d8956d2ee589f89b5
DSMR676 <dmorenor@upao.edu.pe>
2026-07-15 05:00:28 -0500

    seguridad: describir salida hacia ALB

--- infra/security-api-gateway.tf
+++ infra/security-api-gateway.tf
@@ -11,6 +11,7 @@ resource "aws_security_group" "api_gateway_link" {
 resource "aws_vpc_security_group_egress_rule" "api_gateway_to_alb" {
   security_group_id            = aws_security_group.api_gateway_link.id
   referenced_security_group_id = aws_security_group.alb.id
+  description                  = "Salida hacia el balanceador de carga"
   from_port                    = 80
   to_port                      = 80
   ip_protocol                  = "tcp"
